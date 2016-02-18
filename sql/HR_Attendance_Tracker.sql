056f4d2f02e28142d98a4a2a631d7b7a0c4f987b
ITadder <jg248@njit.edu>
2016-02-18 15:30:47 +0000

    My Second Commit

---
+++ sql/HR_Attendance_Tracker.sql
@@ -0,0 +1,25 @@
+# Author: Justino Garcia
+# Class: IT635
+Drop database if exists HRAttendanceTracker;
+create database HRAttendanceTracker;
+use 635Example;
+#drop table if exists `employees`;
+create table `employees` (
+employeesID int(10) primary key not null auto_increment,
+fname varchar(255),
+mname varchar(255),
+lname varchar(255),
+);
+
+insert into `employees`(
+	fname,
+        mname,	
+	lname,
+	values(
+	"Justino",
+	"JR",
+	"Garcia"
+);
+
+
+# adding employees into database	
