c80d22b0bbddc1111d323e99e347ac6c466275e3
ITadder <jg248@njit.edu>
2016-02-21 15:33:37 +0000

    This is my third commit

--- sql/HR_Attendance_Tracker.sql
+++ sql/HR_Attendance_Tracker.sql
@@ -1,11 +1,10 @@
 # Author: Justino Garcia
-# Class: IT635
 Drop database if exists HRAttendanceTracker;
 create database HRAttendanceTracker;
 use 635Example;
 #drop table if exists `employees`;
-create table `employees` (
-employeesID int(10) primary key not null auto_increment,
+create table `employees` 
+(employeesID int(10) primary key not null auto_increment,
 fname varchar(255),
 mname varchar(255),
 lname varchar(255),
