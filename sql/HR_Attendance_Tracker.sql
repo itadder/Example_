b1969004090dbc24f54bb90a8925c0d910239420
Justino Garcia <jg248@njit.edu>
2016-02-21 11:37:31 -0500

    Fixed the .sql script syntax and created a sample record.

--- sql/HR_Attendance_Tracker.sql
+++ sql/HR_Attendance_Tracker.sql
@@ -2,19 +2,20 @@
 # Class: IT635
 Drop database if exists HRAttendanceTracker;
 create database HRAttendanceTracker;
-use 635Example;
-#drop table if exists `employees`;
+use HRAttendanceTracker;
+drop table if exists `employees`;
 create table `employees` (
 employeesID int(10) primary key not null auto_increment,
 fname varchar(255),
 mname varchar(255),
-lname varchar(255),
+lname varchar(255)
+
 );
 
 insert into `employees`(
 	fname,
-        mname,	
-	lname,
+  mname,
+	lname)
 	values(
 	"Justino",
 	"JR",
